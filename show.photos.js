a48fc9626e62bb94de3b7e701a36e3b4d484c5c6
jvverde <jvverde@outlook.com>
2025-02-16 17:08:30 +0000

    v3.2.4

--- show.photos.js
+++ show.photos.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bird Species Image Preview
 // @namespace    http://tampermonkey.net/
-// @version      3.2.3
+// @version      3.2.4
 // @description  Show Flickr images when hovering over bird species names (IOC nomenclature) on a webpage.
 // @author       Isidro Vila Verde
 // @match        *://*/*
